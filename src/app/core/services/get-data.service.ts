5bbf6fb9c616970e85fed37e6fa81df1265ebea7
Dmitry <joydnby@gmail.com>
2020-03-09 20:46:46 +0300

    feat: add property names in getData service in core module

--- src/app/core/services/get-data.service.ts
+++ src/app/core/services/get-data.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { InfoDirector } from './../../directors/models/directors';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetDataService {
+
+  public currentData: InfoDirector[];
+  public currentLanguage: string;
 
   constructor() { }
 }
