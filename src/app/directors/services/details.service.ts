5769197d0582bc0725eee2531fbbffed13f4d3cf
Dmitry <joydnby@gmail.com>
2020-03-09 19:03:22 +0300

    feat: add directorData subject to details service

--- src/app/directors/services/details.service.ts
+++ src/app/directors/services/details.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { InfoDirector } from './../models/directors';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailsService {
-
+  public directorData: Subject<InfoDirector> = new Subject();
   constructor() { }
 }
