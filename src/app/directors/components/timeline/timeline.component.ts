961b5124659ea1fcdd948961e217529e1b6e38ec
alpy86 <alpy86@tut.by>
2020-03-09 15:59:55 +0300

    fix: naming error in timeline component

--- src/app/directors/components/timeline/timeline.component.ts
+++ src/app/directors/components/timeline/timeline.component.ts
@@ -3,11 +3,11 @@ import { HttpService } from 'src/app/core/services/http.service';
 import { BiographyDirector, Directors } from '../../models/directors';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  selector: 'app-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.scss']
 })
-export class TestComponent implements OnInit {
+export class TimelineComponent implements OnInit {
   public dataBiography: Array<BiographyDirector>;
   public alternate: boolean = true;
   public size: number = 40;
