857104f8db5e93547e3876e0b85129079adff811
Dmitry <joydnby@gmail.com>
2020-03-09 20:14:12 +0300

    feat: add searchQuery behavioral subject and queryUpdated method in SearchService

--- src/app/directors/services/search.service.ts
+++ src/app/directors/services/search.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SearchService {
 
+  public searchQuery: BehaviorSubject<string> = new BehaviorSubject<string>('');
   constructor() { }
+
+  public searchQueryUpdated(searchQuery: string): void {
+    this.searchQuery.next(searchQuery);
+  }
+
 }
