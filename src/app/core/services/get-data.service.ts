c85ed1162f67d8546e01b6e565f90e079bfa9dee
Dmitry <joydnby@gmail.com>
2020-03-09 23:15:35 +0300

    feat: add required properties to getData service

--- src/app/core/services/get-data.service.ts
+++ src/app/core/services/get-data.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { InfoDirector } from './../../directors/models/directors';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetDataService {
 
-  public currentData: InfoDirector[];
-  public currentLanguage: string;
+  public currentData: Subject<InfoDirector[]> = new Subject();
+  public currentLanguage: BehaviorSubject<string> = new BehaviorSubject('be');
+
+  // todo: obtain data from assets and pass to currentData;
 
   constructor() { }
 }
--- src/app/directors/pages/directors-list/directors-list.component.ts
+++ src/app/directors/pages/directors-list/directors-list.component.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from './../../services/search.service';
 import { GetDataService } from './../../../core/services/get-data.service';
@@ -13,7 +13,7 @@ export class DirectorsListComponent implements OnInit {
 
   constructor(private getDataService: GetDataService, private searchService: SearchService) { }
 
-  get currentData(): BehaviorSubject<InfoDirector[]> {
+  get currentData(): Subject<InfoDirector[]> {
     return this.getDataService.currentData;
   }
   get currentLanguage(): BehaviorSubject<string> {
