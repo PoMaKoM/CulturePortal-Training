efb066130e72fba0b19dce1e7b9aa4bf759d3a62
Dmitry <joydnby@gmail.com>
2020-03-09 22:56:55 +0300

    feat: add proper data handling and filtering logic in directors list component

--- src/app/directors/pages/directors-list/directors-list.component.ts
+++ src/app/directors/pages/directors-list/directors-list.component.ts
@@ -1,6 +1,8 @@
+import { BehaviorSubject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from './../../services/search.service';
 import { GetDataService } from './../../../core/services/get-data.service';
+import { InfoDirector } from './../../models/directors';
 
 @Component({
   selector: 'app-directors-list',
@@ -8,7 +10,19 @@ import { GetDataService } from './../../../core/services/get-data.service';
   styleUrls: ['./directors-list.component.scss']
 })
 export class DirectorsListComponent implements OnInit {
-  constructor(private getDataService: GetDataService, searchService: SearchService) { }
 
-  public ngOnInit(): void { }
+  constructor(private getDataService: GetDataService, private searchService: SearchService) { }
+
+  get currentData(): BehaviorSubject<InfoDirector[]> {
+    return this.getDataService.currentData;
+  }
+  get currentLanguage(): BehaviorSubject<string> {
+    return this.getDataService.currentLanguage;
+  }
+  get searchQuery(): BehaviorSubject<string> {
+    return this.searchService.searchQuery;
+  }
+
+  public ngOnInit(): void {
+  }
 }
