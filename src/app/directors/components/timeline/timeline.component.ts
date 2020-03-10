7cf56bd441077b4ad0112c7bee7e1008ec8ed7be
Dmitry <joydnby@gmail.com>
2020-03-10 22:50:11 +0300

    fix: timeline connected to detailsService

--- src/app/directors/components/timeline/timeline.component.ts
+++ src/app/directors/components/timeline/timeline.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from 'src/app/core/services/http.service';
+import { DetailsService } from './../../services/details.service';
 import { BiographyDirector } from '../../models/biography-director.model';
 import { Directors } from '../../models/directors.model';
 
@@ -14,11 +14,9 @@ export class TimelineComponent implements OnInit {
   public size: number = 40;
   public side: string = 'left';
 
-  constructor(private httpService: HttpService) { }
+  constructor(private detailsService: DetailsService) { }
 
   public ngOnInit(): void {
-    this.httpService.getDataDirectors()
-      // TODO change request based on language and selected director
-    .subscribe((res: Directors) => this.dataBiography = res.data[0].be.biography);
+    this.dataBiography = this.detailsService.detailsInfo.biography;
   }
 }
