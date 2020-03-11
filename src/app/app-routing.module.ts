55d11b69a33f326bcc52b1e18bdb325f36bc1d2b
Andrei Makarevich <makarka007@gmail.com>
2020-03-11 10:33:53 +0300

    feat: add router path for worklog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: 'main', loadChildren: () => import('./main/main.module').then((m) => m.MainModule) }
+  { path: 'main', loadChildren: () => import('./main/main.module').then((m) => m.MainModule) },
+  { path: 'worklog', loadChildren: () => import('./worklog/worklog.module').then((m) => m.WorklogModule) }
 ];
 
 @NgModule({
