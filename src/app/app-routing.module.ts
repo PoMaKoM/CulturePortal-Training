3f21a50416ed0cb8395eb2ebe4f9ecc1d929e2ad
Dmitry <joydnby@gmail.com>
2020-03-11 18:28:59 +0300

    feat: add routing to directors module in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: 'main', loadChildren: () => import('./main/main.module').then((m) => m.MainModule) }
+  { path: 'main', loadChildren: () => import('./main/main.module').then((m) => m.MainModule) },
+  { path: 'main', loadChildren: () => import('./directors/directors.module').then((m) => m.DirectorsModule) }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
